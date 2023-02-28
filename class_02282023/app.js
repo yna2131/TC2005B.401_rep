203daedc47a63607a23593af63c8028c9819a2b2
A01709043 <A01709043@tec.mx>
2023-02-28 12:41:49 -0600

    Clase de trabajo - 2023.02.28

--- class_02282023/app.js
+++ class_02282023/app.js
@@ -1,2 +1,40 @@
-console.log("Hola desde node");
-console.log("Hola desde nodemon");
+// npm init --> npm을 시작
+// npm install --save-dev nodemon --> nodemon 설치
+// npm install --save express --> express 설치
+// npm start --> 파일 실행
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    
+    //Le permite a la petición avanzar hacia el siguiente mmiddleware
+    next();
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+
+    //Manda la respuesta
+    response.send('¡Hola mundo!');
+});
+
+/* 이건 에러. send를 하고 난 후, 또 한번 next를 하기 때문에 오류가 날 수밖에 없다.
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+
+    //Manda la respuesta
+    response.send('¡Hola mundo!');
+    next();
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+
+    //Manda la respuesta
+    response.send('¡Hola de nuevo!');
+});
+*/
+
+app.listen(3000);
